9c8e3393767e9f03fb8883eb0b07d9069be11416
Owcollier <owcollier@gmail.com>
2018-10-22 14:52:39 -0400

    Basics in place for TopNav component

---
+++ src/Components/TopNav/TopNav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export class TopNav extends React.Component {
+  render() {
+    return (
+      <div className="header-wrapper">
+        <header className="header">
+          
+        </header>
+      </div>
+    );
+  }
+}
+
+export default TopNav;
