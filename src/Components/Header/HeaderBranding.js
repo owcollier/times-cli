d145be2b934d113b5ba9440e96736592e59c6553
Owcollier <owcollier@gmail.com>
2018-10-25 13:53:14 -0400

    HeaderBranding structure in place

--- src/Components/Header/HeaderBranding.js
+++ src/Components/Header/HeaderBranding.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import styles from './Header.module.css';
+import Header from './Header';
+
+export function HeaderBranding(props) {
+
+  return (
+    <div>
+      <div>Smiley</div>
+      <div></div>
+      <div>Hello NYTimes!</div>
+    </div>
+  );
+
+}
+
+export default Header;
